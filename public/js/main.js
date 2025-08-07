5e4e577976bda334e03da326feaeb6dbe3d6092c
Pascal Cescato <pascal.cescato@gmail.com>
2025-08-07 13:48:27 +0200

    nettoyage et correction de l'affichage du message de remerciement

--- public/js/main.js
+++ public/js/main.js
@@ -29,37 +29,7 @@ window.addEventListener('load', function() {
     if ( localStorage.getItem( 'newsletter_submitted' ) === 'true' ) {
         var forms = document.querySelectorAll( '.wpcf7-form' );
         forms.forEach(function(form) {
-            // On ne remplace plus le formulaire par le message si déjà inscrit
             form.style.display = 'none';
         });
-    }
-});
-document.addEventListener( 'wpcf7submit', function( event ) {
-    if ( event.detail.apiResponse && event.detail.apiResponse.mailjet_error ) {
-        console.error( 'Mailjet API Error:', event.detail.apiResponse.mailjet_error );
-    }
-}, false );
-
-document.addEventListener( 'wpcf7mailsent', function( event ) {
-    localStorage.setItem( 'newsletter_submitted', 'true' );
-    var form = event.target;
-    var thankYouMessage = fai_vars.thank_you_message;
-    var thankYouDiv = document.createElement('div');
-    thankYouDiv.innerHTML = thankYouMessage;
-    form.parentNode.replaceChild(thankYouDiv, form);
-}, false );
-
-window.addEventListener('load', function() {
-    if ( localStorage.getItem( 'newsletter_submitted' ) === 'true' ) {
-        var forms = document.querySelectorAll( '.wpcf7-form' );
-        forms.forEach(function(form) {
-            // On ne remplace plus le formulaire par le message si déjà inscrit
-            form.style.display = 'none';
-        });
-        // Optionnel : afficher un message alternatif si besoin
-        // var thankYouMessage = fai_vars.thank_you_message;
-        // var thankYouDiv = document.createElement('div');
-        // thankYouDiv.innerHTML = thankYouMessage;
-        // document.querySelector('.wpcf7').appendChild(thankYouDiv);
     }
 });
